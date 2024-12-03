66439a4e81ac2419d8dc4868b2b711c9324e627e
Quang Bui M. <minhquangbui053@gmail.com>
2024-12-03 11:25:01 +0700

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,9 +10,6 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
