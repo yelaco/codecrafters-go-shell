ab1e804805eb72112f883ccb67baa3b79a4370d4
Quang Bui M. <minhquangbui053@gmail.com>
2024-12-03 15:07:14 +0700

    codecrafters submit [skip ci]

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -3,12 +3,15 @@ package commands
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 type Command struct {
 	cmd  string
 	args []string
+	fn   func(...string)
 }
 
 var ErrCommandNotFound = errors.New("command not found")
@@ -22,23 +25,41 @@ func ParseCommand(s string) (Command, error) {
 		words[i] = strings.TrimSpace(words[i])
 	}
 
-	// Check if command exists
-	if !Exist(words[0]) {
+	fn, exist := GetHandlerIfExist(words[0])
+	if !exist {
 		return Command{}, fmt.Errorf("%s: %w", words[0], ErrCommandNotFound)
 	}
 
 	return Command{
 		cmd:  words[0],
 		args: words[1:],
+		fn:   fn,
 	}, nil
 }
 
-func Exist(cmd string) bool {
+func GetHandlerIfExist(cmd string) (func(...string), bool) {
 	switch cmd {
+	case "exit":
+		return Exit, true
 	default:
-		return false
+		return nil, false
 	}
 }
 
 func (c *Command) Execute() {
+	if c.fn != nil {
+		c.fn(c.args...)
+	}
+}
+
+func Exit(args ...string) {
+	if len(args) == 0 {
+		// TODO: expect arguments
+		return
+	}
+	code, err := strconv.Atoi(args[0])
+	if err != nil {
+		return
+	}
+	os.Exit(code)
 }
